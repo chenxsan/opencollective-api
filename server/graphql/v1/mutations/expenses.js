a5cec2ea0f6485463a3dfd215e29c739bdc00872
Leonardo Kewitz <leo@opencollective.com>
2020-04-03 17:01:36 -0300

    refact: add backward compatible category and tag resolvers

--- server/graphql/v1/mutations/expenses.js
+++ server/graphql/v1/mutations/expenses.js
@@ -120,7 +120,7 @@ const checkExpenseAttachments = (expenseData, attachments) => {
   }
 };
 
-const EXPENSE_EDITABLE_FIELDS = ['amount', 'description', 'category', 'type', 'privateMessage', 'invoiceInfo'];
+const EXPENSE_EDITABLE_FIELDS = ['amount', 'description', 'category', 'type', 'tags', 'privateMessage', 'invoiceInfo'];
 
 const getPaypalPaymentMethodFromExpenseData = async (expenseData, remoteUser, fromCollective, dbTransaction) => {
   if (expenseData.PayoutMethod) {
@@ -209,6 +209,7 @@ export async function createExpense(remoteUser, expenseData) {
       {
         ...pick(expenseData, EXPENSE_EDITABLE_FIELDS),
         currency: collective.currency,
+        tags: expenseData.category ? [expenseData.category] : null,
         status: statuses.PENDING,
         CollectiveId: collective.id,
         FromCollectiveId: fromCollective.id,
@@ -340,17 +341,25 @@ export async function editExpense(remoteUser, expenseData) {
       PayoutMethodId !== expense.PayoutMethodId,
     );
 
-    return expense.update(
-      {
-        ...cleanExpenseData,
-        lastEditedById: remoteUser.id,
-        incurredAt: expenseData.incurredAt || new Date(),
-        status: shouldUpdateStatus ? 'PENDING' : expense.status,
-        PayoutMethodId: PayoutMethodId,
-        legacyPayoutMethod: models.Expense.getLegacyPayoutMethodTypeFromPayoutMethod(payoutMethod),
-      },
-      { transaction: t },
-    );
+    if (cleanExpenseData) {
+      const existingTags = expense.tags || [];
+      let tags = cleanExpenseData.tags;
+      if (cleanExpenseData.category) {
+        tags = [cleanExpenseData.category, ...existingTags];
+      }
+      return expense.update(
+        {
+          ...cleanExpenseData,
+          lastEditedById: remoteUser.id,
+          incurredAt: expenseData.incurredAt || new Date(),
+          status: shouldUpdateStatus ? 'PENDING' : expense.status,
+          PayoutMethodId: PayoutMethodId,
+          legacyPayoutMethod: models.Expense.getLegacyPayoutMethodTypeFromPayoutMethod(payoutMethod),
+          tags,
+        },
+        { transaction: t },
+      );
+    }
   });
 
   await updatedExpense.createActivity(activities.COLLECTIVE_EXPENSE_UPDATED);
