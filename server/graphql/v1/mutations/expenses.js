968b80febe37ffa143df3441c5393d891235f5d3
Leonardo Kewitz <leo@opencollective.com>
2020-04-03 09:46:35 -0300

    style: fix camelcase handleTransferWisePaymentLimit

--- server/graphql/v1/mutations/expenses.js
+++ server/graphql/v1/mutations/expenses.js
@@ -9,7 +9,7 @@ import paymentProviders from '../../../paymentProviders';
 import * as libPayments from '../../../lib/payments';
 import { formatCurrency } from '../../../lib/utils';
 import { floatAmountToCents } from '../../../lib/math';
-import { handleTransferWisePaymentLimit } from '../../../lib/plans';
+import { handleTransferwisePaymentLimit } from '../../../lib/plans';
 import { createFromPaidExpense as createTransactionFromPaidExpense } from '../../../lib/transactions';
 import { getFxRate } from '../../../lib/currency';
 import debugLib from 'debug';
@@ -450,7 +450,7 @@ async function payExpenseWithTransferwise(host, payoutMethod, expense, fees) {
     throw new Error('Host is not connected to Transferwise');
   }
 
-  await handleTransferWisePaymentLimit(host, expense, { throwException: true });
+  await handleTransferwisePaymentLimit(host, expense, { throwException: true });
 
   const data = await paymentProviders.transferwise.payExpense(connectedAccount, payoutMethod, expense);
   const transactions = await createTransactions(host, expense, fees, data);
